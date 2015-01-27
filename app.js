a1d9d5d678f12df7604f8432c2824d96900d247f
shotlom <sholto.maud@gmail.com>
2015-01-27 01:10:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var Busboy = require('busboy'); // to handle the form
+var csv = require('fast-csv');
+var http = require('http'),
+    inspect = require('util').inspect;
+
+http.createServer(function(req, res) {
+  if (req.method === 'POST') {
+
+  var busboy = new Busboy({ headers: req.headers });
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    file.pipe(csv())
+      .on('data', function (data) {
+        console.log('YAY, just the data I wanted!', data);
+      });
+  });
+  busboy.on('finish', function() {
+    console.log('Done parsing form!');
+    	res.writeHead(200);
+	res.end('<html><head></head><body><h1>Done!</h1></body></html>');
+  });
+  req.pipe(busboy);
+} else if (req.method === 'GET') {
+    res.writeHead(200, { Connection: 'close' });
+    res.end('<html><head></head><body>\
+               <form method="POST" enctype="multipart/form-data">\
+                <input type="text" name="textfield"><br />\
+                <input type="file" name="filefield"><br />\
+                <input type="submit">\
+              </form>\
+            </body></html>');
+  }
+}).listen(8000, function() {
+  console.log('Listening for requests');
+});
+
